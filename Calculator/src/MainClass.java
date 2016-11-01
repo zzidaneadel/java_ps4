511b2b0e4065ab53ce59e1ffb223eb9976fe9c58
zzidaneadel <zzidaneadel@gmail.com>
2016-11-01 16:30:37 +0500

    Add files via upload

--- Calculator/src/MainClass.java
+++ Calculator/src/MainClass.java
@@ -4,7 +4,7 @@ public class MainClass {
 	public static void main(String[] args) {
 		
 		Parser parser = new Parser();
-		IArithmetic ar1 = parser.Parse("8-2-");
+		IArithmetic ar1 = parser.Parse("8-2");
 		
 		try {
 			System.out.println(ar1.Calc());
